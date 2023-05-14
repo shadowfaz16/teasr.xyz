989136221b17302d7b7eaf4889cc27c1c288ec24
shadowfaz16 <erikfazju@gmail.com>
2023-05-14 05:34:26 +0100

    changes

---
+++ app/components/Compose.tsx
@@ -0,0 +1,75 @@
+'use client'
+import React from 'react';
+import { useActiveWallet, useCreatePost, ContentFocus, CollectPolicyType } from '@lens-protocol/react-web';
+import { create } from 'ipfs-http-client';
+
+/* configure Infura auth settings */
+const projectId = "2PkzI0wyR3M08EkxAlr4jMXxDZ1"
+const projectSecret = "ee305476603c7936b7ec8bf995a55f59"
+const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
+
+const client = create({
+    host: 'ipfs.infura.io',
+    port: 5001,
+    protocol: 'https',
+    headers: {
+        authorization: auth,
+    }
+})
+
+interface Props {
+    publisher: string;
+}
+
+const Compose = ({ publisher }) => {
+    async function upload(postData: any) {
+        const added = await client.add(JSON.stringify(postData))
+        const uri = `ipfs://${added.path}`
+        console.log('uri: ', uri)
+        return uri
+    };
+    const { execute: create, error, isPending } = useCreatePost({ publisher, upload });
+    const { data: wallet, loading: walletLoading } = useActiveWallet();
+
+    const onSubmit = async () => {
+        await create({
+            content: "My first video",
+            contentFocus: ContentFocus.VIDEO,
+            locale: 'en',
+            collect: {
+                type: CollectPolicyType.FREE,
+                followersOnly: false,
+                metadata: {
+                    name: "livepeer-id",
+                    description: "videoid_559dz7d9mjq27hwr",
+                    attributes: [],
+                }
+            },
+        });
+    };
+    if (isPending) {
+        return <p>Creating post...</p>;
+    }
+    if (error) {
+        console.log(error);
+        return <p>Error: {error.message}</p>;
+    }
+    if (wallet) {
+        console.log(wallet);
+        return (
+            <div>
+                <p>You are logged-in with {wallet.address}</p>
+                <button onClick={onSubmit}>save</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <h1>Homes</h1>
+            <p>You are logged-out</p>
+        </div>
+    );
+}
+
+export default Compose;
--- app/new-teasr/page.tsx
+++ app/new-teasr/page.tsx
@@ -4,23 +4,8 @@ import { useCallback, useMemo, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import Navbar from '../components/NavBar';
 import { FiUpload } from 'react-icons/fi';
-import { create } from 'ipfs-http-client'
-
-import { useActiveProfile, useCreatePost, useActiveWallet, ContentFocus, CollectPolicyType} from '@lens-protocol/react-web';
-
-/* configure Infura auth settings */
-const projectId = "2PkzI0wyR3M08EkxAlr4jMXxDZ1"
-const projectSecret = "ee305476603c7936b7ec8bf995a55f59"
-const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64')
-
-const client = create({
-    host: 'ipfs.infura.io',
-    port: 5001,
-    protocol: 'https',
-    headers: {
-        authorization: auth,
-    }
-  })
+import { useActiveProfile} from '@lens-protocol/react-web';
+import Compose from '../components/Compose';
 
 const NewTeasr = () => {
     const [title, setTitle] = useState("");
@@ -213,62 +198,15 @@ const NewTeasr = () => {
                     )}
                     </div>
                     </div>
-                    <Compose publisher={user} />
+                    {
+                        user && (
+                            <Compose publisher={user} />
+                        )
+                    }
                 </div>
             </div>
         </div>
     );
 };
-
-const Compose = ({ publisher }) => {
-    async function upload(postData:any) {
-        const added = await client.add(JSON.stringify(postData))
-        const uri = `ipfs://${added.path}`
-        console.log('uri: ', uri)
-        return uri
-    };
-    const { execute: create, error, isPending } = useCreatePost({ publisher, upload });
-    const { data: wallet, loading: walletLoading } = useActiveWallet();
-  
-    const onSubmit = async () => {
-      await create({
-        content: "My first video",
-        contentFocus: ContentFocus.VIDEO,
-        locale: 'en',
-        collect: {
-            type: CollectPolicyType.FREE,
-            followersOnly: false,
-            metadata: {
-                name: "livepeer-id",
-                description: "videoid_559dz7d9mjq27hwr",
-                attributes: [],
-            }
-        },
-      });
-    };
-    if (isPending) {
-        return <p>Creating post...</p>;
-    }
-    if (error) {
-        console.log(error);
-        return <p>Error: {error.message}</p>;
-    }
-    if (wallet) {
-      console.log(wallet);
-      return (
-        <div>
-          <p>You are logged-in with {wallet.address}</p>
-          <button onClick={onSubmit}>save</button>
-        </div>
-      );
-    }
-  
-    return (
-      <div>
-        <h1>Homes</h1>
-        <p>You are logged-out</p>
-      </div>
-    );
-  }
 
 export default NewTeasr;
